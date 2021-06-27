1420cd84c92bf0a2679e9a15c22723edc1e23381
LuongXuanTrungDung <luongxuantrungdung211@gmail.com>
2021-06-27 23:01:33 +0700

    Sửa lỗi nhanh

--- src/auth.guard.ts
+++ src/auth.guard.ts
@@ -1,22 +1,20 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Request } from 'express';
-import { Observable } from 'rxjs';
 import { NguoiDungService } from './models/nguoi-dung/nguoi-dung.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private readonly ndSer: NguoiDungService) {}
 
-    canActivate(
+   async canActivate(
         context: ExecutionContext,
-    ): boolean | Promise<boolean> | Observable<boolean> {
+    ) {
         const token = context
             .switchToHttp()
             .getRequest()
             .header('Authorization');
         if (!token || token == '') return false;
 
-        if (this.ndSer.findOne_byID(token)) return true;
+        if (await this.ndSer.findOne_byID(token)) return true;
         else return false;
     }
 }
