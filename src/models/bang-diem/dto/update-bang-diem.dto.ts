87904b47c4044f66df57987c8f8748c6b44356ae
LuongXuanTrungDung <luongxuantrungdung211@gmail.com>
2021-07-12 16:42:56 +0700

    Xóa model bang_diem

--- src/models/bang-diem/dto/update-bang-diem.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBangDiemDto } from './create-bang-diem.dto';
-
-export class UpdateBangDiemDto extends PartialType(CreateBangDiemDto) {}
