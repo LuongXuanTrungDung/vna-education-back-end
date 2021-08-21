f72fd6c96a5879d17902a884ba24fbad2682c7ed
LuongXuanTrungDung <luongxuantrungdung211@gmail.com>
2021-08-22 03:10:12 +0700

    Sửa nhanh

--- src/app.service.ts
+++ src/app.service.ts
@@ -85,7 +85,7 @@ export class AppService {
         }
 
         for (let m = 0; m < temp.length; m++) {
-            temp[m].tietHoc = temp[l].tietHoc.sort((a, b) => {
+            temp[m].tietHoc = temp[m].tietHoc.sort((a, b) => {
                 return sessionSort(a.thuTiet, b.thuTiet);
             });
         }
